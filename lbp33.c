71262078433f3f44943c1a7f5c55d1947a0c0b7a
𝚈𝚘𝚞𝚜𝚞𝚏 <yousufuddin.146@gmail.com>
2025-02-28 19:54:25 +0530

    Add files via upload

--- lbp33.c
+++ lbp33.c
@@ -1,14 +1,27 @@
-#include<stdio.h>
-int doub(int x){
-		return x*2;
-	}
-int main(){
-	int n;
-	printf("Enter the value of n : ");
-	scanf("%d",&n);
-	if(n>=0)
-	printf("2 times of %d is %d",n,doub(n));
-	else
-	printf("Enter a positive value");
-	return 0;
+#include <stdio.h>
+float convertLength(float value,int fu,int su){
+    float meterstofeet = 3.28084;
+    float kilometerstomiles = 0.621371;
+    if (fu==1&&su==2)
+        return value*meterstofeet;
+    else if(fu==2&&su==1)
+        return value/meterstofeet;
+    else if(fu==3&&su==4)
+        return value*kilometerstomiles;
+    else if(fu==4&& su==3)
+        return value/kilometerstomiles;
+    else 
+        return printf("enter other conversions");
+}
+int main() {
+    float value, result;
+    int fu, su;
+    printf("Enter value :\n");
+    scanf("%f",&value);
+    printf("Choose units:\n");
+    printf("1: Meters\n2: Feet\n3: Kilometers\n4: Miles\n");
+    scanf("%d %d",&fu,&su);
+    result = convertLength(value, fu, su);
+    printf("Output: %.2f\n", result);
+    return 0;
 }
