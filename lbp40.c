108c1fb0b823d3c29a6f100a00762438d8584775
𝚈𝚘𝚞𝚜𝚞𝚏 <yousufuddin.146@gmail.com>
2025-03-03 23:34:51 +0530

    Add files via upload

---
+++ lbp40.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int prime(int x){
+	if(x==1){
+		printf("Not Prime Not Composite");
+	}
+	if(x==2){
+		printf("Yes");
+	}
+	for(int i=2;i<x;i++){
+		if(x%i==0) return printf("No");
+		else return printf("Yes");
+	}
+}
+int main(){
+	int n;
+	printf("Enter a number : ");
+	scanf("%d",&n);
+	if(n>0)
+	prime(n);
+	else
+	printf("Enter a postive number : ");
+}
